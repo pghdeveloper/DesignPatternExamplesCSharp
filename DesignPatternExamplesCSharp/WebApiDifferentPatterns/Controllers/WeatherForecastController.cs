83ce7bc59d1b0d0e53b9018d93d9a712415b84c9
pghdeveloper <rmardayat@gmail.com>
2023-07-02 23:18:11 -0400

    Circuit Breaker Pattern

---
+++ DesignPatternExamplesCSharp/WebApiDifferentPatterns/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDifferentPatterns.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    // [HttpGet(Name = "GetWeatherForecast")]
+    // public IEnumerable<WeatherForecast> Get()
+    // {
+    //     
+    //     
+    //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    //         {
+    //             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+    //             TemperatureC = Random.Shared.Next(-20, 55),
+    //             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    //         })
+    //         .ToArray();
+    // }
+    [HttpGet]
+    public async Task<string> Get()
+    {
+        var client = _httpClientFactory.CreateClient("errorAPIClient");
+        return await client.GetStringAsync("api/values");
+    }
+}
